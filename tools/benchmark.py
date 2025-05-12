9f5cd042a3f226eb19b66175c96969d142814bdf
Pekka Rantalankila <pekka.rantalankila@gmail.com>
2025-05-12 13:25:18 +0300

    Revert "Change caseName construction."
    This reverts commit 783bf7340bb17359c0e2460e2c38c3160d97cd78.

--- tools/benchmark.py
+++ tools/benchmark.py
@@ -287,15 +287,7 @@ def convertComparisonData(casePaths, metricSets, gnssConverter):
 
 def benchmarkSingleDataset(benchmark, dirs, vioTrackingFn, args, baselineMetrics=None):
     caseDir = benchmark.dir
-    shortName = benchmark.name
-
-    # This makes sure `caseName` contains the optional custom `rootDataDir` in it.
-    def getCaseName(s):
-        s = s.replace("data/benchmark/", "")
-        s = s.replace("data/confidential/", "")
-        s = s.replace("/", "-")
-        return s
-    caseName = getCaseName(caseDir)
+    caseName = benchmark.name
 
     casePaths = {
         "input": caseDir + "/data.jsonl",
@@ -359,7 +351,6 @@ def benchmarkSingleDataset(benchmark, dirs, vioTrackingFn, args, baselineMetrics
     with open(infoPath, "w") as infoFile:
         infoJson = {
             "caseName": caseName,
-            "shortName": shortName,
             "dir": benchmark.dir,
             "paramSet": benchmark.paramSet,
             "methodName": args.methodName,
